f39e5d2af98380258cc262dc348ec0f0ab8b81a1
yunghavy <kimutaij192@gmail.com>
2024-12-06 15:27:28 +0300

    iskaba

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import heroVideo from "../assets/herovideo3.mp4"; 
+import heroVideo from "../assets/herovideo2.mp4"; 
 import { useNavigate } from "react-router-dom";
 
 
@@ -26,7 +26,7 @@ const Hero = () => {
       {/* Foreground Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full">
         <motion.h1
-          className="text-center text-5xl mt-4 uppercase text-gray-500 tracking-[20px] sm:tracking-[30px] md:tracking-45px]"
+          className="text-center text-5xl mt-4 uppercase text-yellow-500 tracking-[20px] sm:tracking-[30px] md:tracking-45px]"
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
@@ -34,7 +34,7 @@ const Hero = () => {
           YUNG HAVY
         </motion.h1>
         <motion.p
-          className="mt-4 text-lg text-center uppercase text-gray-500 pb-2 tracking-[2px] sm:tracking-[6px] md:tracking-[10px]"
+          className="mt-4 text-lg text-center uppercase text-yellow-500 pb-2 tracking-[2px] sm:tracking-[6px] md:tracking-[10px]"
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.5 }}
@@ -53,7 +53,7 @@ const Hero = () => {
         {["Work", "Scripts", "Reel", "Info", "Art"].map((item) => (
           <span
             key={item}
-            className="text-sm uppercase text-gray-500 tracking-[5px] sm:tracking-[10px] md:tracking-[15px] mx-2 cursor-pointer hover:text-white"
+            className="text-sm uppercase text-yellow-500 tracking-[5px] sm:tracking-[10px] md:tracking-[15px] mx-2 cursor-pointer hover:text-white"
             onClick={() => navigateToPage(item)}
           >
             {item}
